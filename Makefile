03047695b59ea371c6cc20be6a8d4be8925a9ee8
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-03-19 02:59:31 +1100

    finally get a CVDisplayLink working

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: tidy out/metallll
 
 out/metallll: main.m
 	@ mkdir -p out
-	@ clang -framework Cocoa -o $@ $^
+	@ clang -framework Cocoa -framework QuartzCore -o $@ $^
 
 tidy: main.m
 	@ clang-format -i $^
--- main.m
+++ main.m
@@ -1,20 +1,63 @@
 #include <Cocoa/Cocoa.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface MainView : NSView {
+	CVDisplayLinkRef displayLink;
+}
+@end
+
+@implementation MainView
+
+- (id)initWithFrame:(CGRect)frame
+{
+	puts("init");
+	self = [super initWithFrame:frame];
+	self.wantsLayer = YES;
+	self.layer = [CAMetalLayer layer];
+	CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
+	CVDisplayLinkSetOutputCallback(displayLink, displayLinkCallback, self);
+	CVDisplayLinkStart(displayLink);
+	return self;
+}
+
+static CVReturn displayLinkCallback(
+        CVDisplayLinkRef displayLink,
+        const CVTimeStamp* now,
+        const CVTimeStamp* outputTime,
+        CVOptionFlags flagsIn,
+        CVOptionFlags* flagsOut,
+        void* displayLinkContext)
+{
+	MainView* view = (MainView*)displayLinkContext;
+	[view renderFrame:outputTime];
+	return kCVReturnSuccess;
+}
+
+- (void)renderFrame:(const CVTimeStamp*)outputTime
+{
+	CAMetalLayer* metalLayer = (CAMetalLayer*)self.layer;
+	puts("render here");
+}
+
+@end
 
 int main()
 {
 	[NSApplication sharedApplication];
 	[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
 
+	NSRect rect = NSMakeRect(0, 0, 420, 69);
+
 	NSWindow* window = [NSWindow alloc];
 	[window
-	        initWithContentRect:NSMakeRect(0, 0, 420, 69)
-	                  styleMask:NSWindowStyleMaskTitled
+	        initWithContentRect:rect
+	                  styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskResizable
 	                    backing:NSBackingStoreBuffered
 	                      defer:NO];
-	[window setTitle:@"metalllllllllll"];
+	window.title = @"metalllllllllll";
+	window.contentView = [[MainView alloc] initWithFrame:rect];
 
 	[window makeKeyAndOrderFront:nil];
-
 	[NSApp activateIgnoringOtherApps:YES];
 
 	[NSApp run];
