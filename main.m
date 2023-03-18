c7f4dd4322a0da82a335e7089ec32a1537878d84
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-03-19 01:12:44 +1100

    make da window

--- main.m
+++ main.m
@@ -4,5 +4,18 @@ int main()
 {
     [NSApplication sharedApplication];
     [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+    NSWindow* window = [NSWindow alloc];
+    [window
+        initWithContentRect:NSMakeRect(0, 0, 420, 69)
+                  styleMask:NSWindowStyleMaskTitled
+                    backing:NSBackingStoreBuffered
+                      defer:NO];
+    [window setTitle:@"metalllllllllll"];
+
+    [window makeKeyAndOrderFront:nil];
+
+    [NSApp activateIgnoringOtherApps:YES];
+
     [NSApp run];
 }
